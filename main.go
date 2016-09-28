eeae80ad3d3491e428be7c31bdf3375373cd78fd
guozhicheng <com314159@qq.com>
2016-09-28 19:21:07 +0800

    commit

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 	"fmt"
 	"github.com/com314159/godoc_to_markdown_wiki/docparse/tomlparser"
 	"github.com/com314159/godoc_to_markdown_wiki/docparse"
+	"strings"
+	"os"
+	"runtime"
+	"path"
 )
 
 var tomlFile = flag.String("toml","","the toml file path")
@@ -17,12 +21,30 @@ func main()  {
 	
 	flag.Parse()
 	
-	fmt.Println("tomlFile: ",*tomlFile)
-	
 	if *tomlFile == "" {
 		fmt.Println("need toml file path")
 		return
 	}
+	
+	if strings.HasPrefix(*tomlFile,"$GOPATH") {
+		
+		gopath := os.Getenv("GOPATH")
+		dirs := strings.Split(gopath,":")
+		
+		if runtime.GOOS == "windows" {
+			dirs = strings.Split(gopath, ";")
+		}
+		
+		for _,d := range dirs {
+			filePath := strings.Replace(*tomlFile,"$GOPATH",path.Join(d,"src"),1)
+			if _,err := os.Stat(filePath); err == nil {
+				*tomlFile = filePath
+				break
+			}
+		}
+	}
+	
+	fmt.Println("tomlFile: ",*tomlFile)
 	
 	err := tomlparser.ParseToml(*tomlFile)
 	if err != nil {
